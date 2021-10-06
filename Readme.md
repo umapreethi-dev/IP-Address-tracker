b08decbb0de229aff7f1cf598131dc13bc60b7dd
umapreethi <umapreethi123.up@gmail.com>
2021-10-05 20:53:41 -0500

    final commit

---
+++ Readme.md
@@ -0,0 +1,26 @@
+#  IP address tracker
+
+## Project Description
+
+This project helps user to track the ip address and gives location.
+
+## Built with
+
+- HTML5
+- CSS3
+- FlexBox
+- Grid
+- Javascript
+
+### Useful Resources
+
+- [FrontEnd Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0)
+- [Ip Geolocation ApI](https://geo.ipify.org/)
+- [Map Leaflet Js](https://leafletjs.com/)
+
+### Links
+
+- [Demo site](https://umapreethi-dev.github.io/IP-Address-tracker/)
+
+
+
--- index.js
+++ index.js
@@ -1,19 +1,43 @@
-var mymap = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(mymap);
 
-const api_key = "8ebb751f54f2448e9be56ab29e37323a";
-const input = document.getElementById('search').value;
+//Intializing the Map
+let lat = 41.78;
+let lng = -81.147;
+let mymap = new L.map("map");
+mymap.setView(new L.LatLng(lat, lng), 13);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}", {
+  foo: "bar",
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(mymap);
 
-function handleSubmit(event){
-    event.preventDefault();
-    let url = `https://geo.ipify.org/api/v1?apiKey=at_kkLBQJlJJykyO2gzioAwSHQchqoH3&ipAddress=${input}`;
-    fetch(url)
+//Calling the IP Api and Updating the map
+function handleSubmit(event) {
+  const input = document.getElementById("search").value;
+  console.log(input);
+  event.preventDefault();
+  let url = `https://geo.ipify.org/api/v1?apiKey=at_kkLBQJlJJykyO2gzioAwSHQchqoH3&ipAddress=${input}`;
+  console.log(url);
+  fetch(url)
     .then((res) => {
       return res.json();
     })
     .then((data) => {
       console.log(data);
-    })
 
-    
+      document.getElementById("ipAddress").innerHTML = `${data.ip}`;
+      document.getElementById("location").innerHTML = `${data.location.lat}`;
+      document.getElementById("time").innerHTML =  "UTC" + `${data.location.timezone}`;
+      document.getElementById("isp").innerHTML = `${data.as.name}`;
+      lat = `${data.location.lat}`;
+      lng = `${data.location.lng}`;
+      ltn = `${data.location.city}` + "," + `${data.location.country}`;
+      map(lat, lng, ltn);
+    });
+}
+
+//function to update the map
+function map(lat, lng, ltn) {
+  mymap.setView(new L.LatLng(lat, lng), 13);
+  let marker = L.marker([lat, lng]).addTo(mymap);
+  let popup = L.popup().setLatLng([lat, lng]).setContent(ltn).openOn(mymap);
 }
